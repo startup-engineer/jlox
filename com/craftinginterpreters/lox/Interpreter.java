68b49835a9cded9a613307efcd664e1e06540f40
Reet Chowdhary <reetpragya@gmail.com>
2024-09-29 18:58:14 -0400

    parse static methods

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -247,7 +247,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
         Map<String, LoxFunction> methods = new HashMap<>();
         for (Stmt.Function method : stmt.methods) {
-            LoxFunction function = new LoxFunction(method, environment, method.name.lexeme.equals("init"));
+            LoxFunction function = new LoxFunction(
+                method,
+                environment,
+                method.name.lexeme.equals("init")
+            );
             methods.put(method.name.lexeme, function);
         }
 
